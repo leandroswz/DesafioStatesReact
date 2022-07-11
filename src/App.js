29da7528f1a02057e7f185740929b0359e357afc
Leandro Costa <leandrin.costta@gmail.com>
2022-07-11 10:38:17 -0300

    alteracao

--- src/App.js
+++ src/App.js
@@ -2,42 +2,28 @@ import React,{ Component} from 'react';
 import Goiaba from './img/goiaba.jpg'
 import './App.css';
 
-class App extends Component{
-
+export default class App extends React.Component{
+    
   state = {
-    infor: [
-      {
-        nome: "Leandro",
-        idade: 28,
-      },
-      {
-        comida: "Lasanha",
-      },
-      {
-        musica1: "A maior saudade - Henrique e Juliano",
-        musica2: "Pra não dizer que eu não falei do odio - Projota",
-        musica3: "Oceano - Djavan"
-      }
-
-    ]
+      nome: "Leandro Costa",
+      idade: 28,
+      comida: "Lasanha",
+      musicas: ["Paradise", "Viva la vida", "Oceano"]
   }
-
   render(){
-    return(
-      <section className='App'>
-        <h1>Meu nome é : {this.state.infor[0].nome}</h1>
-        <h2>Tenho : {this.state.infor[0].idade} anos</h2>
-        <h3>Comida Preferida : {this.state.infor[1].comida}</h3>
-        <ul>
-          <p>Músicas preferidas:</p>
-          <li>{this.state.infor[2].musica1}</li>
-          <li>{this.state.infor[2].musica2}</li>
-          <li>{this.state.infor[2].musica3}</li>
-        </ul>
-        <p>Fruta Preferida :  Goiaba</p>
-        <img className='fruta' src={Goiaba}/>
-      </section>
-    )
+      return(
+          <div className='App'>
+             <h1>Meu nome é {this.state.nome}</h1>
+             <h2>Tenho {this.state.idade} anos</h2>
+             <h3>Minha comida favorita é : {this.state.comida}!</h3>
+             <ul>
+              <li>{this.state.musicas[0]}</li>
+              <li>{this.state.musicas[1]}</li>
+              <li>{this.state.musicas[2]}</li>
+             </ul>
+
+              <img className='fruta' src={Goiaba} alt="tomate"/>
+          </div>
+      )
   }
 }
-export default App;
